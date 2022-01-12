5676aa2eb4d5a9e66698ecbdeaa9cf51fdec6be5
Courdevelops <courdevelops@gmail.com>
2022-01-11 21:23:07 -0500

    userwID added

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,26 @@ router.get('/', (req, res) => {
 	res.send("User List")
 })
 
+//new user form
 router.get('/new', (req, res) => {
 	//will use post request but for now
 	res.send("User New Form")
+})
+
+//new user form
+router.get('/old', (req, res) => {
+	res.send("Old User Form")
+})
+
+//post request
+router.post('/', (req, res) => {
+    res.send("Create User")
+})
+
+//for getting individual user
+router.get('/:id', (req, res) => {
+    req.params.id
+	res.send(`User Get with ID ${req.params.id}`)
 })
 
 module.exports = router
