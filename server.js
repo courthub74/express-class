ca6706495c33c66a2627f3249327c41cb3237de1
Courdevelops <courdevelops@gmail.com>
2022-01-11 20:54:34 -0500

    with users

---
+++ server.js
@@ -0,0 +1,31 @@
+//Require express library that was downloaded
+const express = require("express")
+
+//set up the server with app variable calling express as a function
+const app = express()
+
+//Tell app to use view engine
+app.set('view engine', 'ejs')
+
+//set the engine and the callback
+// app.engine('ejs', )
+
+//set up routes ("/" - root path)
+app.get("/", (req, res) => {
+    res.render("index", { text: "Your Life will surely change"})
+})
+
+app.get('/users', (req, res) => {
+	//will use post request but for now
+	res.send("User List")
+})
+
+//now generate users
+app.get('/users/new', (req, res) => {
+	//will use post request but for now
+	res.send("User New Form")
+})
+
+
+//make servern run pass through port #
+app.listen(3000)
